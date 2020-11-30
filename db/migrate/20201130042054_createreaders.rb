a8759bde8fe1b4ccc59b16161a8ca29bcb42660b
HaidyF <69495135+HaidyF@users.noreply.github.com>
2020-11-29 23:59:40 -0500

    Sinatra Flash Add

--- db/migrate/20201130042054_createreaders.rb
+++ db/migrate/20201130042054_createreaders.rb
@@ -1,8 +1,10 @@
 class Createreaders < ActiveRecord::Migration
   def change
-    t.string :username
+    create_table :readers do |t|
+      t.string :username
       t.string :email
       t.string :password_digest
       t.integer :book_id
+    end
   end
 end
