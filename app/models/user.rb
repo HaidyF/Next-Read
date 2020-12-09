db02f806c191cba483a7684d0024700587bb2533
HaidyF <69495135+HaidyF@users.noreply.github.com>
2020-12-09 17:54:57 +0200

    dependent

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    has_many :books
+    has_many :books, dependent::destroy
     has_secure_password
     validates_presence_of :username, :email
     validates_uniqueness_of :username, :email
